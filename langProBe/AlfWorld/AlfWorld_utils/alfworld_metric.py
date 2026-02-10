f004e0f44746fada06d94497a5f7d8f771182b4c
Julian Ghadially <13652886+julianghadially@users.noreply.github.com>
2026-02-09 18:02:31 -0600

    hotpotGEPA branch

--- langProBe/AlfWorld/AlfWorld_utils/alfworld_metric.py
+++
@@ -1,5 +0,0 @@
-import dspy
-import os
-
-def alfworld_metric(example: dspy.Example, pred: dspy.Prediction, target: str = None):
-    return pred.success
--- langProBe/IReRa/load_data.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p langProBe/IReRa/data/esco
-cd langProBe/IReRa/data/esco
-git clone https://github.com/jensjorisdecorte/Skill-Extraction-benchmark.git
-cd Skill-Extraction-benchmark
-git checkout 157da05a24e6ecfee82e4b5d01cba68a2ed0552f
-mv * ..
-cd ../../..
-rm -rf langProBe/IReRa/data/esco/Skill-Extraction-benchmark/
