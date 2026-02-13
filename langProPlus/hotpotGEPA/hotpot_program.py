44a8c5a25dcf83d1188b81e9f89a7d9f2458f158
CodeEvolver Agent <codeevolver@anthropic.com>
2026-02-13 11:29:15 +0000

    codeevolver mutation. Date: 20260213100437

--- langProPlus/hotpotGEPA/hotpot_program.py
+++ langProPlus/hotpotGEPA/hotpot_program.py
@@ -2,6 +2,16 @@ import dspy
 from langProBe.dspy_program import LangProBeDSPyMetaProgram
 
 
+class RerankPassages(dspy.Signature):
+    """Rerank passages by relevance to the question and return top 3-5 most relevant passages."""
+
+    question = dspy.InputField()
+    passages = dspy.InputField(desc="List of retrieved passages to rerank")
+    reranked_passages = dspy.OutputField(
+        desc="Top 3-5 most relevant passages as a list, reasoning about which contain the most pertinent information for answering the question"
+    )
+
+
 class GenerateAnswer(dspy.Signature):
     """Answer questions with a short factoid answer."""
 
@@ -18,6 +28,7 @@ class HotpotMultiHopPredict(LangProBeDSPyMetaProgram, dspy.Module):
         self.k = 7
         self.create_query_hop2 = dspy.Predict("question,summary_1->query")
         self.retrieve_k = dspy.Retrieve(k=self.k)
+        self.rerank = dspy.ChainOfThought(RerankPassages)
         self.summarize1 = dspy.Predict("question,passages->summary")
         self.summarize2 = dspy.Predict("question,context,passages->summary")
         self.generate_answer = dspy.Predict(GenerateAnswer)
@@ -25,15 +36,23 @@ class HotpotMultiHopPredict(LangProBeDSPyMetaProgram, dspy.Module):
     def forward(self, question):
         # HOP 1
         hop1_docs = self.retrieve_k(question).passages
-        summary_1 = self.summarize1(
+        # Rerank hop1 passages to filter top 3-5 most relevant
+        reranked_hop1_docs = self.rerank(
             question=question, passages=hop1_docs
+        ).reranked_passages
+        summary_1 = self.summarize1(
+            question=question, passages=reranked_hop1_docs
         ).summary
 
         # HOP 2
         hop2_query = self.create_query_hop2(question=question, summary_1=summary_1).query
         hop2_docs = self.retrieve_k(hop2_query).passages
+        # Rerank hop2 passages to filter top 3-5 most relevant
+        reranked_hop2_docs = self.rerank(
+            question=question, passages=hop2_docs
+        ).reranked_passages
         summary_2 = self.summarize2(
-            question=question, context=summary_1, passages=hop2_docs
+            question=question, context=summary_1, passages=reranked_hop2_docs
         ).summary
 
         # HOP 3: Answer instead of another query+retrieve
