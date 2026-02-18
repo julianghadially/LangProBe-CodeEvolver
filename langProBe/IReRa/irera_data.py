0a445fbd689c9aacaea717a1a39963925d9b4909
Julian Ghadially <13652886+julianghadially@users.noreply.github.com>
2026-02-18 15:29:07 -0600

    redo hover

--- langProBe/IReRa/irera_data.py
+++
@@ -1,9 +0,0 @@
-from ..benchmark import Benchmark
-from .irera_utils import load_data
-
-
-class IReRaBench(Benchmark):
-    def init_dataset(self):
-        (train_examples, validation_examples, test_examples, _, _, _) = load_data()
-        self.dataset = train_examples + validation_examples
-        self.test_set = test_examples
--- langProBe/hover/hover_pipeline.py
+++ langProBe/hover/hover_pipeline.py
@@ -1,19 +1,8 @@
 import dspy
 from langProBe.dspy_program import LangProBeDSPyMetaProgram
-from .hover_program import HoverMultiHopPredict, HoverMultiHop
+from .hover_program import HoverMultiHop
 
 COLBERT_URL = "https://julianghadially--colbert-server-colbertservice-serve.modal.run/api/search"
-
-
-class HoverMultiHopPredictPipeline(LangProBeDSPyMetaProgram, dspy.Module):
-    def __init__(self):
-        super().__init__()
-        self.rm = dspy.ColBERTv2(url=COLBERT_URL)
-        self.program = HoverMultiHopPredict()
-
-    def forward(self, claim):
-        with dspy.context(rm=self.rm):
-            return self.program(claim=claim)
 
 
 class HoverMultiHopPipeline(LangProBeDSPyMetaProgram, dspy.Module):
