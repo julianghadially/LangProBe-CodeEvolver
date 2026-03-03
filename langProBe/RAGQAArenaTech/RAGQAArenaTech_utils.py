e52cc74c1881ecc121b9212014aff7a7cd3a5765
Julian Ghadially <13652886+julianghadially@users.noreply.github.com>
2026-03-02 20:59:46 -0600

    resetting to main

--- langProBe/RAGQAArenaTech/RAGQAArenaTech_utils.py
+++
@@ -1,9 +0,0 @@
-import dspy
-
-
-class GenerateSearchQuery(dspy.Signature):
-    """Write a simple search query that will help answer a complex question based on the information we already have."""
-
-    context = dspy.InputField(desc="may contain relevant facts")
-    question = dspy.InputField()
-    query = dspy.OutputField()
